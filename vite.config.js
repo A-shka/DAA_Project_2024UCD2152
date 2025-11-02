bcb86afd83db8a2574cb6dd5db597984693177f4
A-shka <aashka13.916@yahoo.com>
2025-11-03 03:43:20 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/DAA_PROJECT_2024UCD2152/",
   plugins: [react()],
-  base: '/DAA_Project_2024UCD2152/',
-})
-
+});
